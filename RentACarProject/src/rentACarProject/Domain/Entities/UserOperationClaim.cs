742e6be584dd160a24273c5412ac862e3073e54d
Kerem Özdoğan <keremozdogannn@gmail.com>
2024-06-22 21:11:12 +0300

    inital commit

---
+++ RentACarProject/src/rentACarProject/Domain/Entities/UserOperationClaim.cs
@@ -0,0 +1,9 @@
+﻿using NArchitecture.Core.Security.Entities;
+
+namespace Domain.Entities;
+
+public class UserOperationClaim : UserOperationClaim<Guid, int>
+{
+    public virtual User User { get; set; } = default!;
+    public virtual OperationClaim OperationClaim { get; set; } = default!;
+}
