f5ea06519fa673da40d92722613c0954afc383ae
Kerem Özdoğan <keremozdogannn@gmail.com>
2024-06-22 21:30:04 +0300

    feat: Adding OtpCode

--- RentACarProject/src/rentACarProject/Domain/Entities/OtpAuthenticator.cs
+++ RentACarProject/src/rentACarProject/Domain/Entities/OtpAuthenticator.cs
@@ -3,4 +3,5 @@
 public class OtpAuthenticator : NArchitecture.Core.Security.Entities.OtpAuthenticator<Guid>
 {
     public virtual User User { get; set; } = default!;
+    public int OtpCode { get; set; }
 }
